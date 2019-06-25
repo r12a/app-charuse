471601770d49448cd07de051a69b934a0775b393
r12a <ishida@w3.org>
2019-06-25 19:17:34 +0100

    kmr, Small fixes to notes

--- langs.js
+++ langs.js
@@ -800,11 +800,11 @@ also:
 ['kmr-arab | arab | Naskh style, used in Iran, Iraq, Syria, Lebanon.',
 'kmr-armn | armn | 1921-1929 in Armenia.',
 'kmr-x-yanalif | latn | Yañalif-like alphabet, replaces Armenian alphabet during Latinisation phase (probably 1930s?).',
-'kmr-1928latn | latn | 1928 in Soviet Union, adds some cyrillic characters into a Latin set.',
-'kmr-1929latn | latn | 1929 in Soviet Union.',
+'kmr-x-1928latn | latn | 1928 in Soviet Union, adds some cyrillic characters into a Latin set.',
+'kmr-x-1929latn | latn | 1929 in Soviet Union.',
 'kmr-cyrl | cyrl | From 1946, in Soviet Union &amp; Armenia.',
 'kmr-x-hawar2 | latn | Reduced Hawar alphabet, used in Turkey until 2013 (missing Q,W,X).',
-'kmr--x-uka | latn | Proposed Universal Kurdish alphabet (adds Ł, Ň, Ř and Ü).',
+'kmr-x-uka | latn | Proposed Universal Kurdish alphabet (adds Ł, Ň, Ř and Ü).',
 'kmr-x-yekgirtu | latn | Yekgirtú (Kurdish Unified Alphabet) promoted by the Kurdish Academy of Language for all Kurdish dialects.',]
 },
 
