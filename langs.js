a54b2a614cfb3f29d0799486eb95c845b91ef7c6
r12a <ishida@w3.org>
2019-07-24 06:45:01 +0100

    link synch

--- langs.js
+++ langs.js
@@ -128,7 +128,7 @@ also:
 'azj | latn | From 1992, modified Latin orthography.']
 },
 
-"ban": {  name:"Bali (Balinese)", source:"udhr_ban", region:"seasia", countries:"Indonesia", script:"ascii", speakers:"3300000", local:"Basa Bali /baʃa bali/", 
+"ban": { name:"Bali (Balinese)", source:"5e57fecd469e080a9ee7d6a6ec4ad232405d52ce,udhr_ban", region:"seasia", countries:"Indonesia", script:"ascii", speakers:"3300000", local:"Basa Bali /baʃa bali/", 
 orth:`[Latin] &nbsp; Used since early 20th century.`,
 also:
 ['ban-bali | bali | ',
