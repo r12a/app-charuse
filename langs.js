dec15484c5ac8b7e763a55aa077a488f5753f685
r12a <ishida@w3.org>
2019-08-09 11:59:55 +0100

    +bsq

--- langs.js
+++ langs.js
@@ -238,6 +238,12 @@ also:
 'bs-arab | arab | \'Arebica\', used mainly between the 15th and 19th centuries.']
 },
 
+"bsq": { name:"Bassa", silcode:"bsq", source:"tbd", region:"afr", countries:"Liberia", script:"latn", speakers:"648000", letter:"ɓɔɖɛḾḿṸṹĒēĚěĨĩŃńŨũŪūƁƆƉƐǍǎǏǐǑǒǓǔǸǹÀÁÃÈÉÌÍÒÓÙÚàáãèéìíòóùú", mark:"̀́̃̄̌", local:"Ɓǎsɔ́ɔ̀", 
+orth:`[Latin] &nbsp; Primary usage.`,
+also:
+['bsq-bass | bass | Rediscovered in the Americas at the start of the 20th century. Now used mainly by older people.']
+},
+
 "bsq-bass": { name:"Bassa", silcode:"bsq", source:"feb8d27835761376c21d0c994cb1c128010d1ac0", region:"afr", countries:"Liberia", script:"bass", speakers:"648000", letter:"𖫭𖫪𖫩𖫫𖫬𖫨𖫧𖫥𖫢𖫡𖫗𖫞𖫟𖫙𖫑𖫖𖫘𖫝𖫓𖫣𖫒𖫜𖫚𖫠𖫤𖫔𖫐𖫕𖫛𖫦", mark:"𖫰𖫱𖫲𖫳𖫴", punctuation:"𖫵", local:"𖫔𖫧𖫳𖫒𖫨𖫰𖫨𖫱 (Ɓǎsɔ́ɔ̀)", 
 orth:`[Bassa Vah] &nbsp; Rediscovered in the Americas at the start of the 20th century. Now used mainly by older people.`,
 also:
