17ea9de30618af53b958a580c49d5abf0d71cfd1
r12a <ishida@w3.org>
2021-09-28 06:51:52 +0100

    thai: Synch with orthography notes changes

--- langs.js
+++ langs.js
@@ -2054,7 +2054,7 @@ also:
 "th": { name:"Thai (Siamese)", local:"ภาษาไทย", silcode:"tha", 
 source:"85b37d327a5e470abcc40234d6cbfdfa388d6566,cldr_th,udhr_tha", 
 region:"seasia", countries:"Thailand", script:"thai", speakers:"44000000", 
-letter:"ฯๆกขฃคฅฆงจฉชซฌญฎฏฐฑฒณดตถทธนบปผฝพฟภมยรฤลฦวศษสหฬอฮะาๅำเแโใไ", mark:"์็่้๊๋ัิีึืุู", markaux:"ํ", number:"๐๑๒๓๔๕๖๗๘๙", punctuation:"‐–—‘’“”…′″๏๚๛", symbol:"฿", other:"​⁠", 
+letter:"ฯๆกขฃคฅฆงจฉชซฌญฎฏฐฑฒณดตถทธนบปผฝพฟภมยรฤลฦวศษสหฬอฮะาๅำเแโใไ", mark:"์็่้๊๋ัิีึืุู", markaux:"ํ", number:"๐๑๒๓๔๕๖๗๘๙", punctuation:"‐–—‘’“”…๏๚๛", symbol:"฿", other:"​⁠", aux:"′″",
 orth:`Thai. &nbsp; <a href="../scripts/thai/" target="_blank">Details</a>.`,
 type:"abugida", cs:"no", gpos:"yes", gsub:"yes", cursive:"no", wordsep:"no/zwsp", baseline:"mid", wrap:"word", hyphenation:"no",  direction:"ltr",  justification:"sp ig", 
 vowels:"inh:2 vs:8 vsl:12 base:อ/O venc:5 comp:37 voc:2", 
