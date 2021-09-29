d270bf8d7b8c7dca7b7b4e1c602a080f36c70657
r12a <ishida@w3.org>
2021-09-29 05:36:16 +0100

    + boilerplate.js

---
+++ boilerplate.js
@@ -0,0 +1,40 @@
+az is a macrolanguage that encompasses the following more specific primary language subtags:<br/>South Azerbaijani [azb], North Azerbaijani [azj].
+Legacy applications often use az rather than [azj].
+
+
+
+aii is included in the Syriac [syr] macrolanguage, and legacy applications often use [az] rather azj.
+
+
+
+
+This is an alternate, Balinese-script orthography for Balinese.
+For other orthographies see [emk],
+
+
+ <strong>(shown here)</strong>
+
+
+
+
+"et": { name:"Estonian", redirect:true, source:"", region:"eur", script:"latn", speakers:"0", notes:`et is a macrolanguage that encompasses the following more specific primary language subtags:<br/>Standard Estonian [ekk], Võro [vro]
+Legacy applications often use et rather than [ekk] (Standard Estonian).`},
+
+
+orth:`Latin. &nbsp; Primary usage.`,
+also:
+['kmr-arab | arab | Naskh style, used in Iran, Iraq, Syria, Lebanon.',
+'kmr-armn | armn | 1921-1929 in Armenia.',
+'kmr-x-yanalif | latn | Yañalif-like alphabet, replaces Armenian alphabet during Latinisation phase (probably 1930s?).',
+'kmr-1928latn | latn | 1928 in Soviet Union, adds some cyrillic characters into a Latin set.',
+'kmr-1929latn | latn | 1929 in Soviet Union.',
+'kmr-cyrl | cyrl | From 1946, in Soviet Union &amp; Armenia.',
+'kmr-x-hawar2 | latn | Reduced Hawar alphabet, used in Turkey until 2013 (missing Q,W,X).',
+'kmr--x-uka | latn | Proposed Universal Kurdish alphabet (adds Ł, Ň, Ř and Ü).',
+'kmr-x-yekgirtu | latn | Yekgirtú (Kurdish Unified Alphabet) promoted by the Kurdish Academy of Language for all Kurdish dialects.',]
+
+
+
+orth:`Latin. &nbsp; Primary usage.`,
+also:
+['knc-arab | arab | Naskh style.',]
