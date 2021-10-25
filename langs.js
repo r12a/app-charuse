06d498c30eecc6150384236a54309034af2db3b1
r12a <ishida@w3.org>
2021-10-25 07:36:14 +0100

    he punctuation - add ’” - move „ to punctuationaux

--- langs.js
+++ langs.js
@@ -865,10 +865,10 @@ linked:"arabic/hausa", fonts:"/pickers/arab-ha/",
 
 "haw": { name:"Hawaiian", source:"cldr_haw,udhr_haw", region:"oce", countries:"USA", script:"latn", speakers:"24000", letter:"āēīōūĀĒĪŌŪʻ", mark:"̄", punctuation:"’‘“”", local:" ’Olelo Hawai’i"},
 
-"he": {  name:"Hebrew", local:" עברית‎ (Ivrit), עברית חדשה‎ (ivrít ḥadašá[h])", silcode:"heb", rtl:true, 
-source:"cldr_he,udhr_heb,354bc8cccb82991d56ad51ac6f1c3b8437b28055", 
+"he": { name:"Hebrew", local:" עברית‎ (Ivrit), עברית חדשה‎ (ivrít ḥadašá[h])", silcode:"heb", rtl:true, 
+source:"0a5ae89a710c3b0c9e880e33bdaaf863175ea62a,cldr_he,udhr_heb", 
 region:"wasia", countries:"Israel, etc.", script:"hebr", speakers:"9000000", 
-letter:"אבגדהוזחטיכךלמםנןסעפףצץקרשת", punctuation:"׳״־‐–—", symbol:"₪", other:"\u200D\u200C\u200F\u200E\u2067\u2066\u2069\u202B\u202A\u202C\u034F", markaux:"ְֱֲֳִֵֶַָֹֻּׁׂ", 
+letter:"אבגדהוזחטיכךלמםנןסעפףצץקרשת", punctuation:"׳״־’”‐–—", punctuationaux:"„", symbol:"₪", other:"\u200D\u200C\u200F\u200E\u2067\u2066\u2069\u202B\u202A\u202C\u034F", markaux:"ְֱֲֳִֵֶַָֹֻּׁׂ", 
 orth:`Hebrew. &nbsp;  <a href="../scripts/hebrew" target="_blank">Details</a>.`,
 type:"abjad", cs:"no", gpos:"yes", gsub:"no", cursive:"no", direction:"rtl", wordsep:"space", baseline:"mid", wrap:"word", hyphenation:"yes", hyphen:"-",  justification:"sp",
 vowels:"ml:4 hcm:11",
