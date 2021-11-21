342216c433210937e0434acf66e63006806e9bde
r12a <ishida@w3.org>
2021-11-21 16:03:27 +0000

    eliminate Bamanan duplicate

--- langs.js
+++ langs.js
@@ -340,13 +340,8 @@ vowels:"let:8 vs:7 vsl:13 base:ꪯ/ꪮ/O venc:5 comp:6",
 linked:"taiviet", 
 },
 
-"bm": { name:"Bambara (Bamanankan)", silcode:"bam", source:"61711e9f6eb3f930b12ed40b27bf0d8c2c6ee101,cldr_bm,udhr_bam", region:"afr", countries:"Mali", script:"latn", speakers:"4100000", letter:"ƐƝƆŋŊɛɲɔ", punctuation:"’", local:"bamanankan", 
-orth:`[Latin] &nbsp; `,
-also:
-['nqo | nkoo | Written koiné used by speakers of Maninka, Bambara, Dyula and related dialects.']
-},
-"bm": { name:"Bambara (Bamanankan)", local:"bamanankan", silcode:"bam", 
-source:"cldr_bm,udhr_bam", 
+"bm": { name:"Bamanan (Bambara, Bamanankan)", local:"bamanankan", silcode:"bam", 
+source:"61711e9f6eb3f930b12ed40b27bf0d8c2c6ee101,cldr_bm,udhr_bam", 
 region:"afr", countries:"Mali", script:"latn", speakers:"4100000", 
 letter:"ŋɔɛɲŊƆƐƝʼ", letteraux:"àáèéìíòóùú", markaux:"̀́̂̌", punctuation:"‘’“”", 
 orth:`[Latin] &nbsp; `,
