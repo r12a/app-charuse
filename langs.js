db99565f1230971a25754955a8cf331203c92159
r12a <ishida@w3.org>
2023-05-12 07:21:11 +0100

    bug-bugi: Recalculate character numbers while working on orthography chart.

--- langs.js
+++ langs.js
@@ -512,7 +512,7 @@ also:
 "bug-bugi": { name:"Buginese", local:"ᨅᨔ ᨕᨘᨁᨗ (basa ugi)", silcode:"bug", 
 source:"d997476f6f5d5a7525a2cdf2d0790e944bfa83f2", 
 region:"seasia", countries:"Indonesia, Malaysia", script:"bugi", speakers:"5000000", 
-letter:"ᨀᨁᨂᨄᨅᨆᨈᨉᨊᨌᨍᨎᨐᨑᨒᨓᨔᨖᨃᨏᨋᨇᨕ", mark:"ᨘᨗᨙᨚᨛ", punctuation:"᨞᨟", 
+letter:"ᨀᨁᨂᨃᨄᨅᨆᨇᨈᨉᨊᨋᨌᨍᨎᨏᨐᨑᨒᨓᨔᨕᨖꧏ", mark:"ᨘᨗᨙᨚᨛ", number:"", punctuation:"᨞᨟", symbol:"", other:"\u200D", 
 orth:`[Buginese] &nbsp; No longer in significant use, but found on signage. 
 <a href="../scripts/buginese" target="_blank">Detailed summary</a>.`,
 also:
