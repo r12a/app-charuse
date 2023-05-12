b0c6e992fe08149932f57f6d9fe3966b37954fe7
r12a <ishida@w3.org>
2023-05-12 17:03:56 +0100

    dv-thaa: Recalculate character numbers while working on orthography chart.

--- langs.js
+++ langs.js
@@ -706,7 +706,7 @@ orth:`[Latin] &nbsp; Pinyin-based, used 1984-1990s.`},
 "dv": { name:"Dhivehi (Maldivian, Divehi)", local:"ދިވެހި [d̪iˈʋehi], ދިވެހިބަސް (dhivehi-bas)", silcode:"div", rtl:true, 
 source:"udhr_div,e9188c572c9c158255cede2459f076ecb3b50b9f", 
 region:"sasia", countries:"Maldives", script:"thaa", speakers:"340000", 
-letter:"ޑސމބރގއދޖލހޢނފކށވޙޤތޕޓޔޝޞޅޚޣޒޠޗޏޘޛޟޜޡޥޱ", mark:"ިެްަީުާޮޭޫޯ", punctuation:"،؛", 
+letter:"ހށނރބޅކއވމފދތލގޏސޑޒޓޔޕޖޗޘޙޚޛޜޝޞޟޠޡޢޣޤޥ", mark:"ަާިީުޫެޭޮޯް", number:"", punctuation:"،؛", symbol:"", other:"", 
 orth:`[Thaana] &nbsp;  <a href="../scripts/thaana/" target="_blank">Details</a>.`,
 also:
 ["dv-latn | latn | ‘Maldivian Latin’."],
