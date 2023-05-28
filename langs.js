7853514170bdeb7b20e3e4f2de7ffce92525db42
r12a <ishida@w3.org>
2023-05-28 05:55:42 +0100

    ban-bali: comp -> (5)

--- langs.js
+++ langs.js
@@ -258,7 +258,7 @@ also:
 ["ban | latn | Used since early 20th century.",
 "ban-java | java | No longer in use."],
 type:"abugida", cs:"?", gpos:"yes", gsub:"yes", cursive:"no", direction:"ltr", wordsep:"no/zwsp", baseline:"romn", wrap:"syllable", hyphenation:"(yes)", hyphen:"᭠", justification:"none",
-vowels:"inh:1 ind:10 cm:11 vs:11 base:ᬳ/h pre:2 circ:6 voc:4 comp:5", clusters:"vir stk cnj", medials:"sj:3", finals:"cm:3 vk:᭄", 
+vowels:"inh:1 ind:10 cm:11 vs:11 base:ᬳ/h pre:2 circ:6 voc:4 comp:(5)", clusters:"vir stk cnj", medials:"sj:3", finals:"cm:3 vk:᭄", 
 linked:"bali/ban", 
 },
 
