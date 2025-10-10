841f56470594a22e54539f6d191c6963b4dc06e0
r12a <ishida@w3.org>
2025-10-10 06:17:20 +0100

    + wsg

--- langs.js
+++ langs.js
@@ -2562,6 +2562,14 @@ also:
 linked:"arab/wo", picker:"", font:"Harmattan, 'Scheherazade New'", 
 },
 
+"wsg": { name:"Adilabad Gondi", local:"𑵶𑶓𑶕𑶂𑶋", localtrans:"ɡoːɳɖiː", silcode:"", 
+source:"r12a", 
+region:"sasia", countries:"India", script:"gong", speakers:"300000", 
+letter:"𑵠𑵡𑵢𑵣𑵤𑵥𑵧𑵨𑵪𑵫𑵬𑵭𑵮𑵯𑵰𑵱𑵲𑵳𑵴𑵵𑵶𑵷𑵸𑵹𑵺𑵻𑵼𑵽𑵾𑵿𑶀𑶁𑶂𑶃𑶄𑶅𑶆𑶇𑶈𑶉𑶘", letteraux:"", mark:"𑶊𑶋𑶌𑶍𑶎𑶐𑶑𑶓𑶔𑶕𑶖𑶗", markaux:"", number:"𑶠𑶡𑶢𑶣𑶤𑶥𑶦𑶧𑶨𑶩", numberaux:"", punctuation:"।॥", punctuationaux:"", symbol:"", symbolaux:"", other:"\u{200C}\u{200D}", otheraux:"", aux:"", deprecated:"", 
+orth:`[Gunjala Gondi] Being reactivated from an 18thC script. Telugu and Devanagari are more commonly used. <a href='../scripts/gong/wsg.html' target='_blank'>Details</a>.`,
+linked:"gong/wsg", picker:"gong", font:"Noto Sans Gunjala Gondi", 
+},
+
 "wwa": { name:"Waama (Yoabu)", source:"udhr_wwa", region:"afr", countries:"Benin", script:"latn", speakers:"50000", letter:"ãìàùèÃÌÀÙÈǹƆƐǸũŋŨŊɔɛ", mark:"̃̀", local:"Waama"},
 
 "xh": { name:"Xhosa", silcode:"xho", source:"cldr_xh,udhr_xho", region:"afr", countries:"South Africa", script:"ascii", speakers:"8200000", local:"isiXhosa"},
